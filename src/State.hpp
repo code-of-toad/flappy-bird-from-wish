2aef73bdbb03f50c85c6376d8133731d21b300c3
Danny Han <codeoftoad@gmail.com>
2024-05-24 02:46:37 -0400

    Add missing function param

--- src/State.hpp
+++ src/State.hpp
@@ -5,10 +5,10 @@ namespace ToadCode {
     class State {
     public:
         virtual ~State() = default;
-        virtual void init()         = 0;
-        virtual void handleInput() = 0;
-        virtual void update()       = 0;
-        virtual void draw(float dt) = 0;  // `dt` is for framerate-independent gameplay
+        virtual void init()           = 0;
+        virtual void handleInput()    = 0;
+        virtual void update(float dt) = 0;
+        virtual void draw(float dt)   = 0;  // `dt` is for framerate-independent gameplay
         virtual void pause()        {}
         virtual void resume()       {}
         // NOTE: The `= 0` syntax indicates that the function is "Pure Virtual".
