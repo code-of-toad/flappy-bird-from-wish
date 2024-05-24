25e53eeba2925fd6320f899cc43bd931ceeefb9d
Danny Han <codeoftoad@gmail.com>
2024-05-23 23:14:08 -0400

    Project setup complete

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,13 @@
+#include <SFML/Graphics.hpp>
 #include <iostream>
 
-int Main() {
-    std::cout << "Hello, World!" << std::endl;
+#define SCRN_WIDTH  1200
+#define SCRN_HEIGHT 800
+
+using std::cout, std::endl, std::string, std::vector;
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(SCRN_WIDTH, SCRN_HEIGHT), "sfml works");
+
     return 0;
 }
