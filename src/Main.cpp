d95adacdbb1055a0a5afc8fe5850d49fa56c6d95
Danny Han <codeoftoad@gmail.com>
2024-06-26 20:35:32 -0400

    Archive old attempt

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,13 +1,1 @@
-#include <iostream>
-#include "Game.hpp"
-#include "DEFINITIONS.hpp"
-
-using std::cout, std::endl, std::string;
-
-int main() {
-    ToadCode::Game game(SCREEN_WIDTH,
-                        SCREEN_HEIGHT,
-                        "Flappy Bird From Wish");
-
-    return EXIT_SUCCESS;
-}
+#include <stdio>
