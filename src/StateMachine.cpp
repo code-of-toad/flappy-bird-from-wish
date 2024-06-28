4ff6ffe0083a43bdb95d005156c8c7b5b7684300
Danny Han <codeoftoad@gmail.com>
2024-06-28 03:46:20 -0400

    Tidy up code

--- src/StateMachine.cpp
+++ src/StateMachine.cpp
@@ -18,17 +18,13 @@ namespace cot {
     void StateMachine::processStateChanges() {
         if (_isRemoving && !_states.empty()) {
             _states.pop();
-            if (!_states.empty()) {
-                _states.top()->resume();
-            }
+            if (!_states.empty()) _states.top()->resume();
             _isRemoving = false;
         }
         if (_isAdding) {
             if (!_states.empty()) {
-                if (_isReplacing)
-                    _states.pop();
-                else
-                    _states.top()->pause();
+                if (_isReplacing) _states.pop();
+                else _states.top()->pause();
             }
             _states.push(std::move(_newState));
             _states.top()->init();
