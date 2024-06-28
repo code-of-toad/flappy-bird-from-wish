5853baca3a2034d86f8fd130a80ce19712766693
Danny Han <codeoftoad@gmail.com>
2024-06-28 01:52:40 -0400

    Group the StateMachine, AssetManager, InputManager into a `GameData` struct

--- src/Game.cpp
+++ src/Game.cpp
@@ -9,25 +9,24 @@ namespace cot {
     }
 
     void Game::_run() const {
-        float newTime, frameTime, interpolation;
         float currTime = _clock.getElapsedTime().asSeconds();
-        float accumulator = 0.0f;
+        // float accumulator = 0.0f;
 
         while (_data->window.isOpen()) {
             _data->machine.processStateChanges();
-            newTime = _clock.getElapsedTime().asSeconds();
-            frameTime = newTime - currTime;
+            float newTime = _clock.getElapsedTime().asSeconds();
+            float frameTime = newTime - currTime;
             if (frameTime > 0.25f) frameTime = 0.25f;
             currTime = newTime;
 
-            accumulator += frameTime;
+            float accumulator = frameTime;
             while (accumulator >= _dt) {
                 _data->machine.getActiveState()->handleInput();
                 _data->machine.getActiveState()->update(_dt);
                 accumulator -= _dt;
             }
 
-            interpolation = accumulator / _dt;
+            float interpolation = accumulator / _dt;
             _data->machine.getActiveState()->draw(interpolation);
         }
     }
