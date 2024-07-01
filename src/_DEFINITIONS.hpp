0a1ce8ec475518bb6c09f3abb8ecba502cfe83f7
Danny Han <codeoftoad@gmail.com>
2024-06-30 22:44:55 -0400

    Collect definitions in one file

---
+++ src/_DEFINITIONS.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#define  SCREEN_WIDTH  768
+#define  SCREEN_HEIGHT 1024
+
+#define SPALSH_STATE_SHOW_TIME 3.0
+
+#define SPALSH_SCENE_BACKGROUND_FILEPATH "Assets/res/Splash-Background.png"
